66a21c359f4d846e12a3de06ab107a3d3923e6a4
JohanWRutberg <johan.w.rutberg@gmail.com>
2026-02-28 20:13:10 +0100

    Increase logo size in navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -30,9 +30,9 @@ export function Navbar() {
             <Image
               src="/logo.png"
               alt="Marked by Trobes"
-              width={40}
-              height={40}
-              className="h-10 w-auto"
+              width={60}
+              height={60}
+              className="h-14 w-auto"
             />
             <span className="font-cinzel text-xl font-bold text-navy dark:text-cream">
               Marked by Trobes
